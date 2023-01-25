b65b0e5f545125f7a15584eb29e767dff8456e3b
SugiraVinc <sugiravincentg11@gmail.com>
2023-01-25 16:56:57 +0200

    more str

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ import datetime
 
 class BaseModel:
     def __init__(self, id, created_at, updated_at):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.today()
         self.updated_at = datetime.datetime.now()
 
