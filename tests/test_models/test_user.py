9e5a3c8b5b0233276da3d3dbf87f05b3283c5baa
ell-sie <e.ndiramiye@alustudent.com>
2023-01-26 03:42:07 -0800

    tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -12,25 +12,8 @@ from models.state import State
 
 
 class TestUser(unittest.TestCase):
-    def test_pep8_conformance(self):
-    """Test that the code conforms to PEP8."""
-    pep8style = pep8.StyleGuide(quiet=True)
-    result = pep8style.check_files(['models/user.py'])
-    self.assertEqual(result.total_errors, 0,
-                     "Found code style errors (and warnings).")
 
-def test_user_attributes(self):
-    """
-    Test the attributes of the User class
-    """
-    my_user = User()
-    my_user.first_name = 'John'
-    my_user.last_name = 'Doe'
-    my_user.email = 'johndoe@example.com'
-    self.assertEqual(my_user.first_name, 'John')
-    self.assertEqual(my_user.last_name, 'Doe')
-    self.assertEqual(my_user.email, 'johndoe@example.com')
-def test_pep8_conformance(self):
+    def test_pep8_conformance(self):
     """Test that the code conforms to PEP8."""
     pep8style = pep8.StyleGuide(quiet=True)
     result = pep8style.check_files(['models/user.py'])
