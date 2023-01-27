e8239eeec7464b6179f795d81ab3ecfea4ec9f5b
SugiraVinc <sugiravincentg11@gmail.com>
2023-01-27 13:57:29 +0200

    more

--- models/place.py
+++ models/place.py
@@ -3,10 +3,10 @@ from base_model import BaseModel
 
 
 class Place(BaseModel):
-    city_id = " "
-    user_id = " "
-    name = " "
-    description = " "
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
     number_rooms = 0
     number_bathrooms = 0
     max_guest = 0
--- models/state.py
+++ models/state.py
@@ -3,4 +3,4 @@ from base_model import BaseModel
 
 
 class State(BaseModel):
-    name = " "
+    name = ""
