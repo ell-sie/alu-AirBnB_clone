9b38ad49cec22e17ad0d037c6163516cbf6e9163
SugiraVinc <sugiravincent11@gmail.com>
2023-01-27 01:35:54 -0800

    more

--- models/base_model.py
+++ models/base_model.py
@@ -6,14 +6,14 @@ import datetime
 class BaseModel:
     """The BaseModel class"""
 
-    
+
     def __init__(self, id, created_at, updated_at):
         """the class constructor"""
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = uuid.uuid4()
         self.created_at = datetime.datetime.today()
         self.updated_at = datetime.datetime.now()
-       
+
     def __str__(self):
         """displaying the class name"""
         """return '[{}] ({}) <{}>'\
@@ -21,8 +21,8 @@ class BaseModel:
         print('[{}] ({}) <{}>'.format(type().__name__, self.id, self.__dict__))
 
         def save(self):
-        print(str(self.updated_hour) + ":" + str(self.updated_at.minute)) 
-        
+            print(str(self.updated_hour) + ":" + str(self.updated_at.minute))
+
     def to_dict(self):
         """return dictionary of the class instance"""
         fdict = self.__dict__.copy()
@@ -30,4 +30,3 @@ class BaseModel:
         fdict["update_at"] = (self.updated_at.isoformat())
         fdict["__class__"] = self.__class__.__name__
         return fdict
-
