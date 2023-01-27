09f50499cb94895c8e3dc80d33a2fe1c4f51a6a2
ll-sie <e.ndiramiye@alustudent.com>
2023-01-27 17:45:11 +0200

    test models

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import unittest
+import pep8
+from models.user import User
+
+class TestUser(unittest.TestCase):
+
+
+    def test_pep8_conformance(self):
+     """Test that the code conforms to PEP8 guidelines"""
+    pep8style = pep8.StyleGuide(quiet=True)
+    result = pep8style.check_files(['models/user.py'])
+    self.assertEqual(result.total_errors, 0, "Code does not conform to PEP8")
+
+
+def test_attributes(self):
+    """Test the attributes of the User class"""
+    user = User()
+    user.first_name = 'Agasaro'
+    user.last_name = 'Elsie'
+    user.email = 'ag@alustudent.com'
+    self.assertEqual(user.first_name, 'Agasaro')
+    self.assertEqual(user.last_name, 'Elsie')
+    self.assertEqual(user.email, 'ag@alustudent.com')
