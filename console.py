0eef204bb8b875208d53d023a2bbab987418ba64
ll-sie <e.ndiramiye@alustudent.com>
2023-01-27 12:56:08 +0200

    beautiful code

--- console.py
+++ console.py
@@ -17,17 +17,9 @@ from models.amenity import Amenity
 class HBNBCommand(cmd.Cmd):
 
 
-    """ Access point to command interpreter on console"""
+  """ Access point to command interpreter on console"""
 prompt = '(hbnb)'
-myclass_dict = {
-"BaseModel": BaseModel,
-"User": User,
-"State": State,
-"City": City,
-"Amenity": Amenity,
-"Place": Place,
-"Review": Review
-}
+myclass_dict = {"BaseModel": BaseModel, "User": User, "State": State, "City": City, "Amenity": Amenity, "Place": Place, "Review": Review}
 
 
 def do_nothing(self, arg):
