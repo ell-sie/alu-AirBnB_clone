01bff01bb0648949f07ee6516da6b3c476916dde
SugiraVinc <sugiravincent11@gmail.com>
2023-01-27 01:07:37 -0800

    more

--- console.py
+++ console.py
@@ -2,14 +2,16 @@
 import math
 
 def calculate_circle_area(radius):
+
+
     """
     Given a radius, this function calculates and returns the area of a circle.
     """
     return math.pi * (radius ** 2)
 
+
 def main():
     r = float(input("Enter the radius of the circle: "))
     print("Area of the circle is: {:.2f}".format(calculate_circle_area(r)))
-
 if __name__ == "__main__":
     main()
