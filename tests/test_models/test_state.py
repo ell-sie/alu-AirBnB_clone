9f663abc9962448c8bb3c48abd0d07c64620d226
ell-sie <e.ndiramiye@alustudent.com>
2023-01-29 04:40:07 -0800

    test models

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""test the state"""
+import unittest
+import pep8
+from models.state import State
+
+class TestState(unittest.TestCase):
+def test_pep8_conformance(self):
+"""Test that we conform to PEP8."""
+pep8style = pep8.StyleGuide(quiet=True)
+result = pep8style.check_files(['models/state.py'])
+self.assertEqual(result.total_errors, 0, "Found code style errors.")
+
+def test_class(self):
+    state = State()
+    self.assertEqual(state.__class__.__name__, "State")
+
+def test_state_name(self):
+    state = State()
+    state.name = "Rwanda"
+    self.assertEqual(state.name, 'Rwanda')
+
