21e86b06eaaf6dfe33688409c36e14ac6a33cb1e
ell-sie <e.ndiramiye@alustudent.com>
2023-01-29 13:26:12 -0800

    test engine

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,7 +2,15 @@
 """test for the file storage"""
 import unittest
 import os
+import json
 import pep8
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
