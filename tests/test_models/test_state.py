970652570e249555ef3be65b76aa90b3142a92e3
ll-sie <e.ndiramiye@alustudent.com>
2023-02-01 13:11:11 +0200

    test state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -5,12 +5,18 @@ import pep8
 from models.state import State
 
 class TestState(unittest.TestCase):
+    """Tes the state class"""
 
-  def test_pep8_conformance(self):
-   """Test that we conform to PEP8."""
-pep8style = pep8.StyleGuide(quiet=True)
-result = pep8style.check_files(['models/state.py'])
-self.assertEqual(result.total_errors, 0, "Found code style errors.")
+def test_instance(self):
+        """test instance."""
+        state = State()
+        self.assertIsInstance(state, State)
+
+def test_pep8_conformance(self):
+      """Test that we conform to PEP8."""
+      pep8style = pep8.StyleGuide(quiet=True)
+      result = pep8style.check_files(['models/state.py'])
+      self.assertEqual(result.total_errors, 0, "Found code style errors.")
 
 def test_class(self):
     state = State()
@@ -18,6 +24,6 @@ def test_class(self):
 
 def test_state_name(self):
     state = State()
-    state.name = "Rwanda"
-    self.assertEqual(state.name, 'Rwanda')
+    state.name = ""
+    self.assertEqual(state.name, '')
 
