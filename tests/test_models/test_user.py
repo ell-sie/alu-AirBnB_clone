843096e3715c8180958dab67697f01b1295448d8
ll-sie <e.ndiramiye@alustudent.com>
2023-02-01 13:04:28 +0200

    test user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,19 +6,52 @@ from models.user import User
 class TestUser(unittest.TestCase):
 
 
+    def test_instance(self):
+        """test the instance."""
+        user = User()
+        self.assertIsInstance(user, User)
+
+    
+    def test_is_class(self):
+        """test instance."""
+        user = User()
+        self.assertEqual(str(type(user)),
+                         "<class 'models.user.User'>")
+
+
+    def test_is_subclass(self):
+        """test is_subclass."""
+        user = User()
+        self.assertTrue(issubclass(type(user), BaseModel))
+
+
+    def test_id(self):
+        """test email."""
+        my_user = User()
+        self.assertIsNotNone(my_user.id)
+
+
+    def test_password(self):
+        """test password."""
+        my_user = User()
+        self.assertEqual(my_user.password, "")
+        my_user.password = "root"
+        self.assertEqual(my_user.password, "root")
+
+
     def test_pep8_conformance(self):
-     """Test that the code conforms to PEP8 guidelines"""
-    pep8style = pep8.StyleGuide(quiet=True)
-    result = pep8style.check_files(['models/user.py'])
-    self.assertEqual(result.total_errors, 0, "Code does not conform to PEP8")
+        """Test that the code conforms to PEP8 guidelines"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/user.py'])
+        self.assertEqual(result.total_errors, 0, "Code does not conform to PEP8")
 
 
     def test_attributes(self):
-     """Test the attributes of the User class"""
-    user = User()
-    user.first_name = 'Agasaro'
-    user.last_name = 'Elsie'
-    user.email = 'ag@alustudent.com'
-    self.assertEqual(user.first_name, 'Agasaro')
-    self.assertEqual(user.last_name, 'Elsie')
-    self.assertEqual(user.email, 'ag@alustudent.com')
+        """Test the attributes of the User class"""
+        user = User()
+        user.first_name = 'Betty'
+        user.last_name = 'Bar'
+        user.email = 'airbnb@mail.com'
+        self.assertEqual(user.first_name, 'Betty')
+        self.assertEqual(user.last_name, 'Bar')
+        self.assertEqual(user.email, 'airbnb@mail.com')
