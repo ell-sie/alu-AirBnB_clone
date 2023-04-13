2db19823e4efdbe5cdb948cff3911d7748a6d9a2
Elsie Agasaro Ndiramiye <e.ndiramiye@alustudent.com>
2023-04-13 14:10:32 +0200

    testing amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" testing amenity model """
+""" Unittesting amenity model """
 import unittest
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -8,23 +8,23 @@ from models.base_model import BaseModel
 class TestAmenity(unittest.TestCase):
 
     def test_amenity_creation(self):
-        """ Test if Amenity instance is created correctly. """
+        """ Test if Amenity is created correctly. """
         amenity = Amenity()
         self.assertIsInstance(amenity, Amenity)
 
     def test_is_subclass(self):
-        """ checking if subclass """
+        """ checking if test is_subclass """
         amenity = Amenity()
         self.assertTrue(issubclass(type(amenity), BaseModel))
 
     def test_attribute(self):
-        """ Test if amenity name is set correctly. """
+        """ Test if amenity name is correct. """
         amenity = Amenity()
         amenity.name = "Wifi"
         self.assertEqual(amenity.name, "Wifi")
 
     def test_id(self):
-        """ check id to ensure class inherits"""
+        """ check id to ensure that the class inherits"""
         amenity = Amenity()
         self.assertIsNotNone(amenity.id)
 
